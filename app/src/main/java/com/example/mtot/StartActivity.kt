2abbb1c116c36029a529806266518353a79f79d2
donghoony <aru0504@naver.com>
2023-06-03 12:11:03 +0900

    feat: Login 구현 (절반)

--- app/src/main/java/com/example/mtot/StartActivity.kt
+++ app/src/main/java/com/example/mtot/StartActivity.kt
@@ -6,6 +6,7 @@ import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.mtot.databinding.ActivityStartBinding
 import com.example.mtot.retrofit2.FriendObject
 import com.example.mtot.retrofit2.LoginData
@@ -14,6 +15,8 @@ import com.example.mtot.retrofit2.SharedPreference.saveAccessToken
 import com.example.mtot.retrofit2.SharedPreference.saveMyEmail
 import com.example.mtot.retrofit2.TestMember
 import com.example.mtot.retrofit2.TestMemberList
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -33,21 +36,19 @@ class StartActivity : AppCompatActivity() {
 
         val loginInterface = LoginObject.loginInterface
 
+        val launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+            val task = GoogleSignIn.getSignedInAccountFromIntent(it.data)
+            val account = task.result!!
+            val code = account.serverAuthCode.toString()
+            Log.i("CODE", account.serverAuthCode.toString())
 
-
-        binding.googleButton.setOnClickListener {
-            loginInterface.requestUrl().enqueue(object : Callback<String> {
+            loginInterface.requestUrl(code).enqueue(object : Callback<String> {
 
                 override fun onResponse(call: Call<String>, response: Response<String>) {
                     if(response.isSuccessful){
                         val str = response.body().toString()
                         Log.d("helloraw", str)
                     }
-//                    val str = response.body
-//                    val i = str.indexOf("url")
-//                    val url = str.substring(43+3, str.length-1)
-//                    val index = Intent(Intent.ACTION_VIEW, Uri.parse(url))
-//                    startActivity(index)
                     Log.d("hello", response.toString())
                 }
 
@@ -56,6 +57,39 @@ class StartActivity : AppCompatActivity() {
                 }
             })
 
+        }
+
+        binding.googleButton.setOnClickListener {
+            val gso = GoogleSignInOptions.Builder(
+                GoogleSignInOptions.DEFAULT_SIGN_IN
+            )
+                .requestEmail()
+                .requestProfile()
+                .requestServerAuthCode(getString(R.string.google_client_id))
+                .build()
+            val client = GoogleSignIn.getClient(this, gso)
+            val i = Intent(client.signInIntent)
+            launcher.launch(i)
+//            loginInterface.requestUrl().enqueue(object : Callback<String> {
+//
+//                override fun onResponse(call: Call<String>, response: Response<String>) {
+//                    if(response.isSuccessful){
+//                        val str = response.body().toString()
+//                        Log.d("helloraw", str)
+//                    }
+////                    val str = response.body
+////                    val i = str.indexOf("url")
+////                    val url = str.substring(43+3, str.length-1)
+////                    val index = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+////                    startActivity(index)
+//                    Log.d("hello", response.toString())
+//                }
+//
+//                override fun onFailure(call: Call<String>, t: Throwable) {
+//                    Log.d("hello", t.message.toString())
+//                }
+//            })
+//
         }
 
 //        loginInterface.requestTestMember().enqueue(object : Callback<TestMemberList>{
--- app/src/main/java/com/example/mtot/retrofit2/LoginInterface.kt
+++ app/src/main/java/com/example/mtot/retrofit2/LoginInterface.kt
@@ -5,12 +5,13 @@ import okhttp3.Interceptor
 import okhttp3.Response
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface LoginInterface {
 
-    @GET("/login/oauth")
-    fun requestUrl(
-    ): Call<String>
+    @GET("/login/oauth2/code/google")
+    fun requestUrl(@Query("code") code: String): Call<String>
 
     @GET("/members/all")
     fun requestTestMember(
