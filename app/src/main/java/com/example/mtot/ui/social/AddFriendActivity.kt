8ac25854ad375b5d0bedf8f65d16dcd882175899
febbyviolen <febbyviolen@gmail.com>
2023-06-05 01:24:19 +0900

    AddFriendActivity 완료요

--- app/src/main/java/com/example/mtot/ui/social/AddFriendActivity.kt
+++ app/src/main/java/com/example/mtot/ui/social/AddFriendActivity.kt
@@ -4,10 +4,12 @@ import android.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.activity.addCallback
 import com.example.mtot.databinding.ActivityAddFriendBinding
 import com.example.mtot.retrofit2.AddFriend
 import com.example.mtot.retrofit2.FriendEmailData
+import com.example.mtot.retrofit2.getMyEmail
 import com.example.mtot.retrofit2.getRetrofitInterface
 import org.json.JSONObject
 import retrofit2.Call
@@ -18,6 +20,7 @@ class AddFriendActivity : AppCompatActivity() {
     lateinit var binding: ActivityAddFriendBinding
     lateinit var addFriend: AddFriend
     var alertDialog: AlertDialog? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAddFriendBinding.inflate(layoutInflater)
@@ -26,47 +29,51 @@ class AddFriendActivity : AppCompatActivity() {
     }
 
     fun init() {
+        val retrofitInterface = getRetrofitInterface()
 
         binding.ivAddFriendBack.setOnClickListener {
             finish()
         }
+        val user_id = getMyEmail(this)
+        binding.userId.text = user_id
 
         binding.buttonAddFriend.setOnClickListener {
-
-            val retrofitInterface = getRetrofitInterface()
             val friendEmail = binding.editTextText.text.toString()
             Log.d("hello", friendEmail)
 
             retrofitInterface.addFriend(FriendEmailData(friendEmail)).enqueue(object : Callback<AddFriend> {
                 override fun onResponse(call: Call<AddFriend>, response: Response<AddFriend>) {
-                    Log.d("hello", response.body().toString())
+                    if (response.isSuccessful) {
+                        addFriend = response.body()!!
+                        if (addFriend == null) {
+                            val dialog = AlertDialog.Builder(this@AddFriendActivity)
+                                .setTitle("친구 추가 요청 발송 실패")
+                                .setMessage("친구 ID가 없습니다.")
+                                .setPositiveButton("확인") { dialog, _ ->
+                                    dialog.dismiss()
+                                    binding.editTextText.text.clear()
+                                }
+                                .create()
+                            alertDialog = dialog
+                            alertDialog?.show()
+                        } else {
+                            val message = "친구 추가 요청 발송했습니다\nid: $addFriend"
+                            Toast.makeText(this@AddFriendActivity, message, Toast.LENGTH_SHORT).show()
+                            finish()
+                        }
+                    } else {
+                        val dialog = AlertDialog.Builder(this@AddFriendActivity)
+                            .setTitle("친구 추가 요청 발송 실패")
+                            .setMessage("친구 ID가 없습니다.")
+                            .setPositiveButton("확인") { dialog, _ ->
+                                dialog.dismiss()
+                                binding.editTextText.text.clear()
+                            }
+                            .create()
+                        alertDialog = dialog
+                        alertDialog?.show()
+                    }
                 }
-
-
-//                    if (response.isSuccessful) {
-//                        addFriend = response.body()!!
-//                        val dialog = AlertDialog.Builder(this@AddFriendActivity)
-//                            .setTitle("친구 추가 요청 발송했습니다")
-//                            .setMessage("id: " + addFriend.toString())
-//                            .setPositiveButton("확인") { dialog, _ ->
-//                                dialog.dismiss() // Dismiss the dialog before finishing the activity
-//                                finish()
-//                            }
-//                            .create()
-//                        alertDialog = dialog
-//                        alertDialog?.show()
-//                    } else {
-//                        val dialog = AlertDialog.Builder(this@AddFriendActivity)
-//                            .setTitle("아이디가 없습니다")
-//                            .setPositiveButton("확인") { dialog, _ ->
-//                                dialog.dismiss() // Dismiss the dialog before finishing the activity
-//                                binding.editTextText.text.clear()
-//                            }
-//                            .create()
-//                        alertDialog = dialog
-//                    }
-//                }
-
                 override fun onFailure(call: Call<AddFriend>, t: Throwable) {
                     Log.d("hello", t.message.toString())
                 }
